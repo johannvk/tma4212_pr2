afd4b2656f76ed1381b63782cd1c8f9060df7ea5
johannvk <johannesvk@protonmail.com>
2020-03-22 16:32:12 +0100

    Made convergence plots

--- testing_reaction_diffusion.py
+++ testing_reaction_diffusion.py
@@ -15,33 +15,59 @@ from typing import Union, Callable, Tuple
 
 from solvers import DiffusionReactionSolver1D, DiffusionReactionSolver2D
 
+# font = {'family' : 'normal',
+#         'weight' : 'bold',
+#         'size'   : 22}
+#
+# matplotlib.rc('font', **font)
+plt.rcParams.update({'font.size': 15})
+
 
 def one_dim_diffusion_reaction_convergence(u_exact: Callable, mu: float, f: Callable,
                                            Neumann_BC : Union[None, Tuple[float, float]] = None,
-                                           M: int = 200, X : Tuple[float, float] = (0.0, 1.0), T: float = 1.0):
+                                           M: int = 100, X : Tuple[float, float] = (0.0, 1.0),
+                                           N: int = 100, T: float = 1.0):
     xs = np.linspace(X[0], X[1], M)
     exact_solution_T = u_exact(xs, T)
 
     u_init = u_exact(xs, 0.0)
 
     Ns = np.logspace(1.0, 2.2, 10, dtype=int)
-    sup_errors = np.zeros(len(Ns))
+    Ms = np.logspace(1.0, 2.2, 10, dtype=int)
+    time_sup_errors = np.zeros(len(Ns))
+    space_sup_errors = np.zeros(len(Ms))
 
-    for i, N in enumerate(Ns):
-        RD_solver_N = DiffusionReactionSolver1D(u_init, xs, mu, f, N=N, T=T, X=X, Neumann_BC=Neumann_BC)
-        solution_array = RD_solver_N.execute()
-        sup_errors[i] = np.max(np.abs(exact_solution_T - solution_array[-1, :]))
+    for i, n in enumerate(Ns):
+        RD_solver_N = DiffusionReactionSolver1D(u_init, xs, mu, f, N=n, T=T, X=X, Neumann_BC=Neumann_BC)
+        solution_array_n = RD_solver_N.execute()
+        time_sup_errors[i] = np.max(np.abs(exact_solution_T - solution_array_n[-1, :]))
 
-    print(f"Sup-errors: {sup_errors}\n")
+    for i, m in enumerate(Ms):
+        m_xs = np.linspace(X[0], X[1], m)
+        m_u_init = u_exact(m_xs, 0.0)
+        RD_solver_M = DiffusionReactionSolver1D(m_u_init, m_xs, mu, f, N=N, T=T, X=X, Neumann_BC=Neumann_BC)
+        solution_array_m = RD_solver_M.execute()
+        sup_m_error = np.max(np.abs(u_exact(m_xs, T) - solution_array_m[-1, :]))
+        space_sup_errors[i] = sup_m_error
+
+    print(f"Space sup-errors: {space_sup_errors}\n")
+    print(f"Time sup-errors: {time_sup_errors}\n")
+
     Ns -= 1  # To get the precise value of 'h' when taking the reciprocal.
-    log_log_slope = linregress(np.log10(1/Ns), np.log10(sup_errors))[0]
+    time_slope = linregress(np.log10(1 / Ns), np.log10(time_sup_errors))[0]
 
-    fig, ax = plt.subplots(1, 1)
+    Ms -= 1
+    space_slope = linregress(np.log10(1/Ms), np.log10(space_sup_errors))[0]
 
-    ax.plot(np.log10(1/Ns), np.log10(sup_errors), label=f"Sup-error(N): Slope {log_log_slope:.3f}")
-    ax.set_xlabel("log(h = 1/N)")
-    ax.set_ylabel("log(Sup|u_ex - u_num|)")
-    ax.legend(loc="best")
+    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+    ax.set_title("Convergence Plot, Neumann Boundary")
+    ax.plot(np.log10(1/Ns), np.log10(time_sup_errors), linewidth=2.5, label=f"Decreasing k, Slope {time_slope:.3f}, h={(X[1]-X[0])/(M-1):.2e}")
+    ax.plot(np.log10(1/Ms), np.log10(space_sup_errors), linewidth=2.5, label=f"Decrasing h, Slope {space_slope:.3f}, k={T/(N-1):.2e}")
+
+    ax.set_xlabel("log(step length)")
+    ax.set_ylabel("log(Sup|u - U|)")
+    ax.legend(loc="best", fontsize=12)
+    plt.tight_layout()
     plt.show()
 
 
@@ -51,15 +77,15 @@ def test_one_dim_dirichlet_convergence():
     def u_exact(x, t):
         return np.exp(-np.pi**2*t)*np.sin(np.pi*x) - t*x*(1.0 - x)
     mu = 1.0
-    f = lambda x, t, u: 1.0*(-x * (1.0 - x) - 2*t)
+    f = lambda x, t, u: -x * (1.0 - x) - 2*t
 
     one_dim_diffusion_reaction_convergence(u_exact=u_exact, mu=mu, f=f)
 
 
 def test_one_dim_dirichlet():
     print("Testing one-dimensional Neumann boundary conditions.\n")
-    M, N = 200, 200
-    X, T = (0.0, 1.0), 5.0
+    M, N = 160, 200
+    X, T = (0.0, 1.0), 0.1
 
     xs = np.linspace(X[0], X[1], M)
 
@@ -90,32 +116,54 @@ def test_one_dim_dirichlet():
 
 def test_one_dim_neumann_convergence():
     print("Testing one-dimensional Neumann Convergence.\n")
-    def u_exact(x, t):
-        return t**3 + x*(1-x)
 
-    def f(x, t, u): return 3*t**2 + 1.0
+    # def u_exact(x, t):
+    #     return t**2 + x*(1-x)
+    #
+    # def f(x, t, u): return 2*t + 1.0
+    # mu = 0.5
+    # neumann_bc = (1.0, -1.0)
+
+    x0, L = 0.0, 2.0
+    X, T = (x0, L), 12.0
+
     mu = 0.5
 
-    one_dim_diffusion_reaction_convergence(u_exact, mu, f, Neumann_BC=(1.0, -1.0))
+    def u_exact(x, t):
+        return np.sin(x) + np.cos(t)
+
+    def f(x, t, u):
+        return -np.sin(t) + mu * np.sin(x)
+
+    neumann_bc = (np.cos(x0), np.cos(L))
+
+    one_dim_diffusion_reaction_convergence(u_exact, mu, f, Neumann_BC=neumann_bc, X=X, T=T)
 
 
 def test_one_dim_neumann():
     print("Testing one-dimensional Neumann boundary conditions.\n")
-
-    def u_exact(x, t):
-        return t**3 + x*(1-x)
-
-    def f(x, t, u): return 3*t**2 + 1.0
-
-    M, N = 200, 25
-    X, T = (0.0, 1.0), 1.0
+    x0, L = 0.0, 2.0
+    M, N = 20, 20
+    X, T = (x0, L), 1.0
 
     mu = 0.5
+
+    def u_exact(x, t):
+        return np.sin(x) + np.cos(t)
+
+    def f(x, t, u):
+        return -np.sin(t) + mu*np.sin(x)
+
+    # def u_exact(x, t):
+    #     return t**3 + x*(1-x)
+    #
+    # def f(x, t, u): return 3*t**2 + 1.0
+
     xs = np.linspace(X[0], X[1], M)
 
     u_init = u_exact(xs, 0.0)
     exact_solution_T = u_exact(xs, T)
-    neumann_bc = (1.0, -1.0)
+    neumann_bc = (np.cos(x0), np.cos(L))
 
     RD_solver_Neumann = DiffusionReactionSolver1D(u_init, xs, mu, f, N=N, T=T, X=X, Neumann_BC=neumann_bc)
     solution_array = RD_solver_Neumann.execute()
@@ -232,14 +280,14 @@ def test_2_two_dim_neumann():
 
 
 def test_batch():
-    test_one_dim_dirichlet_convergence()
-    test_one_dim_dirichlet()
+    # test_one_dim_dirichlet_convergence()
+    # test_one_dim_dirichlet()
 
     test_one_dim_neumann_convergence()
-    test_one_dim_neumann()
+    # test_one_dim_neumann()
 
-    test_1_two_dim_neumann()
-    test_2_two_dim_neumann()
+    # test_1_two_dim_neumann()
+    # test_2_two_dim_neumann()
 
 
 if __name__ == '__main__':
